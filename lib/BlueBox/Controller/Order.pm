f59ee44e0f2b9d7a72a0dce1029a21b30df0da2d
Henry Van Styn <vanstyn@intellitree.com>
2014-05-03 20:56:48 -0400

    More work on Order controller

--- lib/BlueBox/Controller/Order.pm
+++ lib/BlueBox/Controller/Order.pm
@@ -1,7 +1,8 @@
 package BlueBox::Controller::Order;
 use Moose;
 use namespace::autoclean;
-use JSON qw(decode_json);
+
+use RapidApp::Include qw(sugar perlutil);
 
 BEGIN { extends 'Catalyst::Controller'; }
 
@@ -22,36 +23,63 @@ Catalyst Controller for ordering new boxes.
 
 =cut
 
+sub now_dt { DateTime->now( time_zone => 'local' ) }
+sub dtf { }
+
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
     
-    my $loc_id = $c->req->params->{location_id} || $self->get_new_location($c);
-
-    $c->response->body('Matched BlueBox::Controller::Order in Order.');
+    my $Account = $c->model('DB::Account')->current_Account or die "Not logged in.";
+    my $Location = $self->get_location($c,$Account)
+    
+    my $params = $c->req->params;
+    
+    my $loc_id = $params->{location_id} || $self->get_new_location($c);
+    my $qty = $params->{qty} or die "qty required";
+    
+    my $dtf = $c->model('DB')->schema->storage->datetime_parser;
+    
+    my $Order = $c->model('DB::Order')->create({
+      account_id  => $Account->get_column('id'),
+      location_id => $Location->get_column('id'),
+      qty         => $qty,
+      ordered_on  => $dtf->format_datetime( &now_dt )
+    })->get_from_storage or die "Error creating order"
+    
+    # TODO: dispatch to a real view
+    $c->res->content_type('application/json; charset=utf-8');
+    $c->res->header('Cache-Control' => 'no-cache');
+    $c->res->status(200);
+    $c->res->body(encode_json_utf8({ $Order->get_columns }));
+    return $c->detach;
 }
 
 
 sub get_location {
-  my ( $self, $c ) = @_;
+  my ( $self, $c, $Account ) = @_;
   
-  my $Account = $c->model('DB::Account')->current_Account 
-    or die "Not logged in."
+  $Account ||= $c->model('DB::Account')->current_Account or die "Not logged in.";
+  my $acc_id = $Account->get_column('id');
   
   my $Location;
   my $Rs = $c->model('DB::Location');
   my $loc_id = $c->req->params->{location_id};
   if($loc_id) {
-    $Location = $Rs->search_rs({ 'me.id' => $loc_id })->first
-      or die "Location ID '$loc_id' not found";
+    $Location = $Rs->search_rs({ 
+      'me.id'         => $loc_id,
+      'me.account_id' => $acc_id
+    })->first or die "Location ID '$loc_id' not found";
   }
   else {
     my $loc_json = $c->req->params->{loc_json} or die "No location info supplied";
-    my $loc = decode_json($loc_json);
-    $Location = $Rs->create($loc) or die "Error creating new location";
+    my $loc = decode_json_uft8($loc_json);
+    $Location = $Rs->create({
+      %$loc,
+      account_id => $acc_id
+    }) or die "Error creating new location";
   }
   
   return $Location;
-  
 }
 
 
